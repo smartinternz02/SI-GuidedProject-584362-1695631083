e9dcbc07279f320be3b68794bb2fe073520a7c15
anjaliarora1312 <123932567+anjaliarora1312@users.noreply.github.com>
2023-09-28 16:21:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ import requests
 
 app = Flask(__name__)
 app.secret_key = 'a'
-conn = ibm_db.connect("DATABASE=bludb; HOSTNAME=1bbf73c5-d84a-4bb0-85b9-ab1a4348f4a4.c3n41cmd0nqnrk39u98g.databases.appdomain.cloud; PORT=32286; UID=wsc79439;PASSWORD=r6yH4fLwtIPrqFY9; SECURITY=SSL;SSLServerCertificate = DigiCertGlobalRootCA.crt", "", "")
+conn = ibm_db.connect("DATABASE=bludb; HOSTNAME=1bbf73c5-d84a-4bb0-85b9-ab1a4348f4a4.c3n41cmd0nqnrk39u98g.databases.appdomain.cloud; PORT=32286; UID=wsc79439;PASSWORD=r6yH4fLwtIPrqFY9; SECURITY=SSL;SSLCertificate = DigiCertGlobalRootCA.crt", "", "")
 url = "https://rapidprod-sendgrid-v1.p.rapidapi.com/mail/send"
 @app.route("/")
 def index():
